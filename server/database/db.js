ed44e78b66b2a708a335e9e38fccb455ba605ebe
Nishtha Sikri <nishthasikri654@gmail.com>
2026-03-16 21:55:56 +0530

    fixed database connection for neon and vercel

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_8FGTh3LWRXJa@ep-wandering-violet-a4g0yd8v-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
